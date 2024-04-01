7fe7fccb919e33af3c1291404d1983c8332c1498
Addis <addisale27@gmail.com>
2024-04-01 20:31:16 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import StarRating from "./starRating";
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
-      <p>this movie was rated {movieRating}stars</p>
-    </div>
-  );
-}
+import "./index.css";
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* {<App />} */}
-    <StarRating maxRating={10} />
-    <StarRating
-      maxRating={5}
-      size={24}
-      className="test"
-      message={[`Terrible`, `Bad`, `Okay`, `Good`, `Amazing`]}
-      retingDefault={3}
-      color="red"
-    />
-    <Test />
+    <App />
   </React.StrictMode>
 );
