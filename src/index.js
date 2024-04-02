4990c8d0aa74a2a2b1cf9d912738a10874f5ebbc
Addis <addisale27@gmail.com>
2024-04-02 19:57:19 +0300

    star rating component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import StarRating from "./starRating";
+//import "./index.css";
+//import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxStar={10} />
   </React.StrictMode>
 );
