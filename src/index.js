be622cd6f2eb92025c3349949cd69335b92565f8
Addis <addisale27@gmail.com>
2024-04-02 20:30:20 +0300

    compltetly creating the star

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import StarRating from "./starRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxStar={10} />
+    <StarRating maxRating={10} />
+    <StarRating />
   </React.StrictMode>
 );
