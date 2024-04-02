e9933d0687660ea7e0deb5b432f36b3656d8235f
Addis <addisale27@gmail.com>
2024-04-02 22:37:12 +0300

    just amazing

--- src/starRating.js
+++ src/starRating.js
@@ -1,19 +1,21 @@
 import { useState } from "react";
-
 const containerStyle = {
   display: "flex",
-  alignItems: "center",
   gap: "10px",
 };
 const ratingStyle = { display: "flex" };
-const textStyle = { lineHeight: "1", margin: 0 };
-const starStyle = {
-  width: "48px",
-  height: "48px",
-  cursor: "pointer",
-  display: "block",
-};
-export default function StarRating({ maxRating = 5 }) {
+
+export default function StarRating({
+  maxRating = 5,
+  color = `#FCC419`,
+  size = 48,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: 0,
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
   const [rating, setRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
   function handleOnRate(rating) {
@@ -36,6 +38,8 @@ export default function StarRating({ maxRating = 5 }) {
             onMoveIn={() => handleMoveIn(i + 1)}
             onMoveOut={() => handleMoveOut()}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            color={color}
+            size={size}
           />
         ))}
       </div>
@@ -43,8 +47,13 @@ export default function StarRating({ maxRating = 5 }) {
     </div>
   );
 }
-function Star({ onRate, full, onMoveIn, onMoveOut }) {
-  console.log(full);
+function Star({ onRate, full, onMoveIn, onMoveOut, color, size }) {
+  const starStyle = {
+    width: `${size / 1.5}px`,
+    height: "48px",
+    cursor: "pointer",
+    display: "block",
+  };
   return (
     <span
       style={starStyle}
@@ -57,8 +66,8 @@ function Star({ onRate, full, onMoveIn, onMoveOut }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -67,7 +76,7 @@ function Star({ onRate, full, onMoveIn, onMoveOut }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
